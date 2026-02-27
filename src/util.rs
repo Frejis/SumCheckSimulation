f35c7895d61ec8344ceb97c8da5ec8b558ede25f
Carl-Emil H. Andersen <ceha@live.dk>
2026-02-27 11:58:15 +0100

    Wrote a function that creates a line between two vectors of fields.

--- src/util.rs
+++ src/util.rs
@@ -41,3 +41,41 @@ pub fn random_gate<F: Field>(label_length: usize) -> Vec<F> {
     }
     res
 }
+
+
+/// Takes two vectors representing the values picked for b*, c* in thalers 2025 opionated paper.
+///
+/// # Arguments
+///
+/// * `b_star`: The first fixed gate.
+/// * `c_star`: The second fixed gate.
+/// * `t`: The point on the line.
+///
+/// returns: Vec<F, Global>
+/// Idk global has to be there otherwise it doesn't show F.
+pub fn _line_point<F: Field>(b_star: &[F], c_star: &[F], t: F) -> Vec<F> {
+    assert_eq!(b_star.len(), c_star.len());
+    b_star
+        .iter()
+        .zip(c_star.iter())
+        .map(|(b, c)| {
+            *b + t * (*c - *b)
+        })
+        .collect()
+}
+
+#[cfg(test)]
+mod test {
+    use ark_bls12_381::Fr;
+    use ark_ff::{One, Zero};
+    use crate::util::_line_point;
+
+    #[test]
+    fn test_line_point() {
+        let size = 5;
+        let b_star = vec![Fr::zero(); size];
+        let c_star = vec![Fr::one(); size];
+        assert_eq!(_line_point(b_star.as_ref(), c_star.as_ref(), Fr::zero()), b_star);
+        assert_eq!(_line_point(b_star.as_ref(), c_star.as_ref(), Fr::one()), c_star)
+    }
+}
