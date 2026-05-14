9358b061b1cc238718adedcc95e6e445b9d643b2
Carl-Emil <ceha@live.dk>
2026-05-14 11:58:29 +0200

    Rng is now handled at the callee level to ensure the caller has control over how the randomness is distributed.

--- src/gkr/gkr_circuit.rs
+++ src/gkr/gkr_circuit.rs
@@ -1,5 +1,4 @@
 use ark_ff::Field;
-use ark_poly::{DenseMultilinearExtension, SparseMultilinearExtension};
 use ark_std::test_rng;
 use rand::Rng;
 use crate::gkr::layer::Layer;
@@ -9,17 +8,16 @@ impl<F: Field> GKRCircuit<F> {
     /// Generate a random layered circuit with the given sizes.
     /// Each element of `layer_sizes` must be a power of 2.
     /// Æast layer will be the input layer.
-    /// Should only be used for testing as it uses test_rng()
     pub fn random<R: Rng>(layer_sizes: &[usize], rng: &mut R) -> Self {
         assert!(layer_sizes.len() >= 2);
         let mut circuit_layer: Vec<Layer> = Vec::new();
         // This will be generated from bottom to top to easily generate the gates.
         for i in 0..layer_sizes.len() - 1 {
             let mut layer = Vec::new();
-            for j in 0..layer_sizes[i] {
+            for _ in 0..layer_sizes[i] {
                 let gate_predicate: GateType = random_gate_type();
-                let left_input: usize = random_number(layer_sizes[i +1]);
-                let right_input: usize = random_number(layer_sizes[i +1]);
+                let left_input: usize = random_number(layer_sizes[i +1], rng);
+                let right_input: usize = random_number(layer_sizes[i +1], rng);
                 let gate = Gate::new(left_input, right_input, gate_predicate);
                 layer.push(gate);
             }
@@ -29,8 +27,8 @@ impl<F: Field> GKRCircuit<F> {
     }
 }
 
-fn random_number(max: usize) -> usize {
-    test_rng().gen_range(0..max)
+fn random_number<R: Rng>(max: usize, rng: &mut R) -> usize {
+    rng.gen_range(0..max)
 }
 
 fn random_gate_type() -> GateType {
