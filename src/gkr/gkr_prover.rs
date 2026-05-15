a012d26dd87e4be6bec8d223585f4c9e22fbed2a
Carl-Emil H. Andersen <ceha@live.dk>
2026-05-15 15:43:53 +0200

    Removed unused imports and fields.

--- src/gkr/gkr_prover.rs
+++ src/gkr/gkr_prover.rs
@@ -1,15 +1,12 @@
 use ark_ff::Field;
-use ark_poly::{DenseMultilinearExtension, MultilinearExtension, SparseMultilinearExtension};
+use ark_poly::{DenseMultilinearExtension, SparseMultilinearExtension};
 use crate::gkr::gkr_driver::log2_pow2;
-use crate::gkr::layer::{EvaluatedLayer, InputLayer, Layer};
+use crate::gkr::layer::InputLayer;
 use crate::gkr::predicates::{AddPredicate, MultPredicate};
-use crate::structures;
-use crate::structures::circuit_structures::{EvaluatedGKRCircuit, GKRCircuit, Gate, GateType};
-use crate::structures::data_structures::SumCheckProver;
+use crate::structures::circuit_structures::{EvaluatedGKRCircuit, GKRCircuit, GateType};
 
 pub struct GKRProver<F: Field> {
     circuit: GKRCircuit<F>,
-    evaluated_circuit: EvaluatedGKRCircuit<F>,
     predicates: Vec<(AddPredicate<F>, MultPredicate<F>)>, // For each layer in the circuit.
     input: InputLayer<F>,
 }
@@ -28,7 +25,6 @@ impl<F: Field> GKRProver<F> {
     pub fn new(circuit: GKRCircuit<F>, input: InputLayer<F>) -> Self {
         Self {
             circuit,
-            evaluated_circuit: EvaluatedGKRCircuit::empty(),
             predicates: Vec::new(),
             input,
         }
@@ -48,14 +44,14 @@ impl<F: Field> GKRProver<F> {
             let s_i = log2_pow2(layer.gates.len());
             let next_s_i = log2_pow2(self.get_next_layer_address_space(i));
 
-            // For the predicate we need to create a index of (g, b, c)
+            // For the predicate we need to create an index of (g, b, c)
             // Where g is the gate index, b is the left child and c is the right child.
             // This is g is the first index then b and then c so it works when fixing variables.
 
             // TODO: Refactor below into a method that makes sense.
             for (gate_idx, gate) in layer.gates.iter().enumerate() {
                 let left_index = gate.left << s_i;
-                let right_index = (gate.right << s_i + next_s_i);
+                let right_index = gate.right << s_i + next_s_i;
                 let index: usize = gate_idx | left_index | right_index;
                 match gate.predicate {
                     GateType::Add => add_terms.push((index, F::one())),
@@ -78,7 +74,7 @@ impl<F: Field> GKRProver<F> {
     }
 
     fn get_next_layer_address_space(&self, i: usize) -> usize {
-        return if i == self.circuit.layers.len() - 1 {
+        if i == self.circuit.layers.len() - 1 {
             self.input.values.len()
         } else {
             self.circuit.layers[i + 1].gates.len()
